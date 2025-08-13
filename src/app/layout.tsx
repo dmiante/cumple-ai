f50023b59f2d1768f46a71d80ed77b7b80a77799
dmiante <dmiantedev@gmail.com>
2025-08-12 21:25:40 -0400

    add sonner and fix format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,32 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type {Metadata} from 'next'
+
+import {Geist, Geist_Mono} from 'next/font/google'
+import './globals.css'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin']
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
-  title: "CumpleAI",
-  description: "Hace mensajes de cumpleaños para enviar, descubre eventos historicos que pasó en el dia de tu cumpleaños y encuentra que regalos gratis de tiendas para ese día",
-};
+  title: 'CumpleAI',
+  description:
+    'Hace mensajes de cumpleaños para enviar, descubre eventos historicos que pasó en el dia de tu cumpleaños y encuentra que regalos gratis de tiendas para ese día'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
